5b9a3ff7534a24a1422787feb3a14b5472e4200a
Rafael Brito <rushbig@hotmail.com>
2023-04-13 22:09:01 -0300

    Adicionando ex02 - converte Styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { GlobalStyles } from './styles'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+<>
+    <GlobalStyles/>
+    <App />
+</>
+)
